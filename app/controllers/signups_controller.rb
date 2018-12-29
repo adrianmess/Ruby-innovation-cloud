80d7d9f835e04a2cf303ef15ba737d13fa0cae64
Adrian Messado <adrian.messado@gmail.com>
2018-12-28 19:51:44 -0800

    add private method to collect data from form and update the signup model

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -2,4 +2,11 @@ class SignupsController < ApplicationController
 	def new
 		@signup = Signup.new
 	end
+
+	private
+	def signup_params
+		#collect data from form and update signup model
+		#here we require the model name signup and permit the column name email
+		params.require(:signup).permit(:email)
+	end
 end
